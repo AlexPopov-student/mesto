f41321d59454c47d6ed648ef60bf6adce92e5dc9
Alexey Popov <alexey.popov@sportsensus.ru>
2021-05-21 12:25:55 +0300

    Fix: Исправить ошибки первой попытки 4-ой Проектной работы

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,39 +1,30 @@
 //прописываем все используемеые переменные
-let openPopup = document.querySelector('#popup');
-    popupToggle = document.querySelector('#popup_opened');
-    popupClose = document.querySelector('.popup__closemark');
+let popup = document.querySelector('#popup');
+    popupOpen = document.querySelector('#popup_opened');
+    popupClose = document.querySelector('#popup__closemark');
     formElement = document.querySelector('.edit-form');
     nameInput = document.querySelector('.profile__title');
     jobInput = document.querySelector('.profile__subtitle');
     anewName = document.querySelector('#name');
     anewOccupation = document.querySelector('#occupation');
     editForm = document.querySelector('.edit-form__button');
-    likeCurrent = document.querySelectorAll('.card__like-button');
+    likeCurrent = document.querySelectorAll('.like-button_normal');
 console.log(likeCurrent);
  
 //открываем форму
-popupToggle.onclick = function(){
-    popup.style.display="initial";
-};
-
+popupOpen.addEventListener('click', toggleClass)
 //закрываем форму
-popupClose.onclick = function(){
-    popup.style.display="none";
-};
-
-//в принципе, этого нет в брифе, но пусть закрывается ещё и по нажатию на любое место вне формы редактирования. Если не нравится - я удалю. Просто в учебных целях
-    window.onclick = function(evt){
-    if (evt.target == popup) {
-        popup.style.display="none";
-    }
-};
+popupClose.addEventListener('click',toggleClass)
+//функция-переключатель
+function toggleClass(){
+    popup.classList.toggle('popup_opened');
+}
 
 //включаем "сердечки"
-
 function toggleLike (evt){
     evt.preventDefault();
     if (evt.target === evt.currentTarget){
-        evt.target.classList = 'card__like-button_active';
+        evt.target.classList = 'like-button_active';
     }
 }
     for (i = 0; i< likeCurrent.length; i++) {
@@ -41,7 +32,6 @@ function toggleLike (evt){
 }
 
 //а теперь редактируем формочки 
-
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     nameInput.textContent = anewName.value;
