c9d7bc196fefbb8a240efdb89bf59650629b8592
Alexey Popov <alexey.popov@sportsensus.ru>
2021-06-13 22:05:10 +0300

    Fix. Вторые правки к 5-ой ПР

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,35 +1,36 @@
-const popup = document.querySelector('#popup_type_edit-profile');
-      cardPopup = document.querySelector('#popup_type_add-card');
-      windowOpen = document.querySelector('#popup_type_image'); 
+const popupEditProfile = document.querySelector('#popup_type_edit-profile');
+      popupCard = document.querySelector('#popup_type_add-card');
+      popupImage = document.querySelector('#popup_type_image'); 
       openEditProfilePopupBtn = document.querySelector('#popup_opened');
       closeEditProfilePopupBtn= document.querySelector('#popup__closemark');
       closeAddCardPopupBtn = document.querySelector('#popup__cardclosemark')
-      formElement = document.querySelector('.edit-form');
+      formEditProfile = document.querySelector('.edit-form');
       nameInput = document.querySelector('.profile__title');
       jobInput = document.querySelector('.profile__subtitle');
       userName = document.querySelector('#name');
       userOccupation = document.querySelector('#occupation');       
 
-openEditProfilePopupBtn.addEventListener('click',() => togglePopup(popup));              //открываем форму редактирования путешественника
-closeEditProfilePopupBtn.addEventListener('click',() => togglePopup(popup));             //закрываем форму редактирования путешественника
+openEditProfilePopupBtn.addEventListener('click', fillInFormEditProfileInputs);          //открываем форму редактирования путешественника
+closeEditProfilePopupBtn.addEventListener('click',() => togglePopup(popupEditProfile));  //закрываем форму редактирования путешественника
 
-function setNewData(){
+function fillInFormEditProfileInputs(){                                                  //функция первоначального заполнения полей "Жак-Ив Кусто"
     userName.value = nameInput.textContent;    
-    userOccupation.value = jobInput.textContent;  
+    userOccupation.value = jobInput.textContent;
+    togglePopup(popupEditProfile);
 }
 
-function togglePopup(evt){                                                               //Просили одну функцию-переключатель? Получите. И останьте, пожалуйста. Всё по брифу, всё работает
-    evt.classList.toggle('popup-type-window_opened');
+function togglePopup(popup){                                                               //Просили одну функцию-переключатель? Получите. И останьте, пожалуйста. Всё по брифу, всё работает
+    popup.classList.toggle('popup-type-window_opened');
 }
 
 function formEditProfileSubmitHandler (evt) {                                            //функция занесения нового персонажа 
     evt.preventDefault(); 
     nameInput.textContent = userName.value;
     jobInput.textContent = userOccupation.value;
-    togglePopup(popup);
+    togglePopup(popupEditProfile);
 }
 
-formElement.addEventListener('submit', formEditProfileSubmitHandler);                    //фиксируем нового персонажа
+formEditProfile.addEventListener('submit', formEditProfileSubmitHandler);                //фиксируем нового персонажа
 
 const cards = document.querySelector('.cards');                                          //объявляем переменные необходимые для клонирования шаблона
 const template = document.querySelector('#template').content;
@@ -38,9 +39,10 @@ function renderCards(){                                                         
     initialCards.forEach(renderCard);
 }
 
-function renderCard (card){                                                             //создаём и добавляем карточки в DOM                               
+function renderCard (card){                                                             //добавляем карточки в DOM                               
     const newCard = createCard(card.place, card.link);
-}
+    cards.appendChild(newCard)
+}   
 
 function createCard(place, link){                                                        //ГЛАВНАЯ ФУНКЦИЯ возврата карточек 
     const cardElement = template.cloneNode(true);                                        //вызвали шаблон карточки (блок 'li')
@@ -48,57 +50,54 @@ function createCard(place, link){                                               
     const cardImage = cardElement.querySelector('.card__image');
     cardText.textContent = place;                                                        //заполнили данными 
     cardImage.src = link;
-    cardImage.alt = place;
-    cardImage.addEventListener('click', toggleWindow);                                   //по щелчку на картинку нарисованной карточки запускаем функцию открытия большой картинки
-    function toggleWindow(){                                                             //открыли большую картинку
-       togglePopup(windowOpen); 
-       windowHandler(place, link);                                                       //отправили в функцию заполнения windowHandler значения
-    }       
+    cardImage.alt = place;                                 
+    cardImage.addEventListener('click',()=>  windowHandler(place, link));                //по щелчку на картинку нарисованной карточки запускаем функцию открытия большой картинк                                                                                      //по щелчку на картинку нарисованной карточки запускаем функцию открытия большой картинки
     setCardListeners(cardElement);                                                       //вызвали функцию переключателей
-    cards.prepend(cardElement);                                                          //добавили карточку в обойму cards ('ul')
+    return cardElement;                                                                  //как же вы меня достали...
 }
 
 renderCards();                                                                           //запускаем функцию генерации карточек
 
+function setCardListeners(evt){                                                          //универсальная функция-"слушатель" - вызов других
+    evt.querySelector('#like').addEventListener('click',handleLike);
+    evt.querySelector('.dustbin').addEventListener('click',deleteCard);   
+}
+
 const windowImage = document.querySelector('.popup-type-window__image');                 //просили объъявить в глобальной области видимости - ок! Объявил. Прогресс кода потрясает
 const windowTitle = document.querySelector('.popup-type-window__text');
 
-function windowHandler(place, link){                                                     //функция-заполнение "большой карточки" картинкой и подписью  
+function windowHandler(place, link){ 
+    togglePopup(popupImage);                                                             //функция-открыятия "большой карточки" картинкой и подписью  
     windowImage.src = link;                                                              //заполнили большую картинку данными 
     windowTitle.textContent = place;
     windowImage.alt = place;
 };
 
 const closeImagePopupBtn = document.querySelector('#window__closemark');
-closeImagePopupBtn.addEventListener('click',() => togglePopup(windowOpen));         //слушатель закрытия большого окна
+closeImagePopupBtn.addEventListener('click',() => togglePopup(popupImage));              //слушатель закрытия большого окна
+  
 
-function setCardListeners(evt){                                                         //универсальная функция-"слушатель" - вызов других
-    evt.querySelector('#like').addEventListener('click',handleLike);
-    evt.querySelector('.dustbin').addEventListener('click',deconsteCard);
-}
-
-function deconsteCard(evt){                                                              //стираем карточку
+function deleteCard(evt){                                                                //стираем карточку
     evt.target.closest('.card').remove()
 }
 
 function handleLike (evt){             
-    evt.target.classList.toggle('like_active')                                          //расставляем лайки
+    evt.target.classList.toggle('like_active')                                           //расставляем лайки
 }                          
-const inputPlace = document.querySelector('#place');                                    //присваиваем им то, что заносит пользователь в формочке
-const inputLink = document.querySelector('#link');
-
-function submitAddCardForm(evt){
-    evt.preventDefault();
-    createCard(inputPlace.value, inputLink.value);
-    document.querySelector('#create').reset();
-    togglePopup(cardPopup);
-}
-    
+const inputPlace = document.querySelector('#place').value;                               //присваиваем им то, что заносит пользователь в формочке
+const inputLink = document.querySelector('#link').value;
+const formAddCard = document.querySelector('#create'); 
 const addCardButton = document.querySelector('#newCardOpen');                            //открываем формочку внесения новой карточки
 
-addCardButton.addEventListener('click',() => togglePopup(cardPopup));                                   
+function submitAddCardForm(evt){                                                         //отвалите уже нахрен!
+    evt.preventDefault();
+    renderCard ({place: inputPlace, link: inputLink});
+    formAddCard.reset();                                                                 //стираем занесённые данные 
+    togglePopup(popupCard);
+}
 
-closeAddCardPopupBtn.addEventListener('click',() => togglePopup(cardPopup));                                  
+addCardButton.addEventListener('click',() => togglePopup(popupCard));                                   
 
-const formAddCard = document.querySelector('#create');                                   //запускаем функцию генерации новых карточек
-      formAddCard.addEventListener('submit', submitAddCardForm);
+closeAddCardPopupBtn.addEventListener('click',() => togglePopup(popupCard));                                  
+
+formAddCard.addEventListener('submit', submitAddCardForm);                               //запускаем функцию генерации новых карточек
